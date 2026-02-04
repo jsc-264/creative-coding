c189f496257ea61d75086c5997e11469cd54ce60
jsc-264 <joestephen264@gmail.com>
2026-02-04 14:14:07 +0000

    added face features in drawFace function

--- face/sketch.js
+++ face/sketch.js
@@ -1,13 +1,17 @@
-function drawFace(x, y, d){
+function drawFace(x, y, d) {
   push()
   translate(x, y)
+  circle(0, 0, d) // head
+  circle(-d / 5, -d / 5, d / 5) // left eye
+  circle(d / 5, -d / 5, d / 5) // right eye
 
-  circle(0, 0, d)
+  arc( 0, d / 8, 2 * d / 5, 2*d / 5, 0, -PI) // smile
   pop()
 }
 
 function setup() {
   createCanvas(600, 600);
+  rectMode(CENTER)
 }
 
 function draw() {
