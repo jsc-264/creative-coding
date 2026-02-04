b861d2a87a9ebbb22f46227c343bf5831394bce7
jsc-264 <joestephen264@gmail.com>
2026-02-04 14:08:49 +0000

    init drawFace func

--- face/sketch.js
+++ face/sketch.js
@@ -1,7 +1,17 @@
+function drawFace(x, y, d){
+  push()
+  translate(x, y)
+
+  circle(0, 0, d)
+  pop()
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
 }
 
 function draw() {
   background(220);
+
+  drawFace(200, 300, 200)
 }
