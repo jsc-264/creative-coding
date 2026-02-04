d65538641f96fd126ba3c497dd0f9e1e5e4b2b23
jsc-264 <joestephen264@gmail.com>
2026-02-04 15:05:38 +0000

    made grid of squares

--- grid/sketch.js
+++ grid/sketch.js
@@ -1,7 +1,20 @@
+const DIM = 4
+let tileSize
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  rectMode(CENTER)
+
+  tileSize = width / DIM
 }
 
 function draw() {
   background(220);
+
+  for (let i = 0; i < DIM; i++) {
+    for (let j = 0; j < DIM; j++) {
+      noFill()
+      square(tileSize * i + tileSize/2, tileSize * j + tileSize/2, tileSize * 0.9)
+    }
+  }
 }
