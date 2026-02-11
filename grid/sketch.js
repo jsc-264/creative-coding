9ff7f0f45bd397d736ec2e9632a08f1465d66cc1
jsc-264 <joestephen264@gmail.com>
2026-02-11 12:02:48 +0000

    added glow to mouse pos

--- grid/sketch.js
+++ grid/sketch.js
@@ -1,6 +1,6 @@
 const DIM = 100
 let tileSize
-
+const rad = 100
 
 function setup() {
   createCanvas(600, 600);
@@ -18,7 +18,18 @@ function draw() {
       let y = tileSize * j
 
       let hue = map(i, 0, DIM, 0, 360)
-      let brg = map(j, 0, DIM, 0, 100)
+      let brg = map(j, 0, DIM, 10, 90)
+      const d = dist(mouseX, mouseY, x, y)
+
+      if (d < rad){
+        // hue = (hue + 180) % 360
+        brg = map(d, 0, rad, 100, brg)
+      }
+
+      // if (d < rad){
+      //   const rel = d / rad
+      //   hue = map(rel, 1, 0, 360, hue)
+      // }
 
       fill(hue, 100, brg)
 
