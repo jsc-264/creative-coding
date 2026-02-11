a1c4ac5fcae75f1b82a02ae714405c334412a7cd
jsc-264 <joestephen264@gmail.com>
2026-02-11 14:39:05 +0000

    created grid in canvas

--- vera-molnar/sketch.js
+++ vera-molnar/sketch.js
@@ -1,7 +1,24 @@
+const DIM = 10
+let w
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  rectMode(CENTER)
+  angleMode(DEGREES)
+
+  w = width / DIM
 }
 
 function draw() {
   background(220);
+
+  for (let i = 0; i < DIM; i++) {
+    for (let j = 0; j < DIM; j++) {
+      const x = w * i + w / 2
+      const y = w * j + w / 2
+      drawSquare(x, y, w)
+    }
+  }
+
+  noLoop()
 }
