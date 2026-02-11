7120cdfaec5e8725422e61d7485d6969973357e7
jsc-264 <joestephen264@gmail.com>
2026-02-11 15:05:27 +0000

    made moving ball using perlin noise

--- perlin/sketch.js
+++ perlin/sketch.js
@@ -1,7 +1,19 @@
+let x, y
+let xoff = 1
+let yoff = 2
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(50);
+  x = noise(xoff) * width
+  y = noise(yoff) * width
+
+  noStroke()
+  circle(x, y, 40, 40)
+
+  xoff += 0.02
+  yoff += 0.02
 }
