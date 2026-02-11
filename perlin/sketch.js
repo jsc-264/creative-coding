b8ea06bfefa2f1fdb38c1ae4b74b653f126a3c53
jsc-264 <joestephen264@gmail.com>
2026-02-11 15:42:37 +0000

    made perlin transfer through noise space

--- perlin/sketch.js
+++ perlin/sketch.js
@@ -1,42 +1,27 @@
-const DIM = 50
-let w
-let inc
-
-let xstart, ystart
-
-let xoff, yoff
+let inc = 0.01
+let start = 0
 
 function setup() {
   createCanvas(400, 400);
-  w = width / DIM
-
-  xstart = random(5)
-  ystart = random(5)
-  inc = 0.01
 }
 
 function draw() {
   background(50);
-  
-  yoff = ystart
-  for (let i = 0; i < DIM; i++) {
-    
-    xoff = xstart
-    for(let j = 0; j < DIM; j++) {
-      const x = i * w
-      const y = j * w
-      
-      noStroke()
-      fill(noise(xoff, yoff) * 255)
-      rect(x, y, w)
 
-      xoff += inc
-    }
+  let offset = start
 
-    yoff += inc
+  noFill()
+  stroke(200)
+  beginShape()
+  for (let x = 0; x < width; x++) {
+    const y = noise(offset) * height
+    vertex(x, y)
+
+    offset += inc
   }
+  endShape()
 
-  xstart += inc
-  ystart += inc
+  start += inc
+
   // noLoop()
 }
