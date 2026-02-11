085796d7e9a43cfbfb0909d6facf0b8bcf117cf2
jsc-264 <joestephen264@gmail.com>
2026-02-11 15:32:32 +0000

    made moving 2d perlin

--- perlin/sketch.js
+++ perlin/sketch.js
@@ -1,20 +1,42 @@
-const DIM = 13
+const DIM = 50
 let w
+let inc
+
+let xstart, ystart
+
+let xoff, yoff
 
 function setup() {
   createCanvas(400, 400);
   w = width / DIM
 
+  xstart = random(5)
+  ystart = random(5)
+  inc = 0.01
 }
 
 function draw() {
   background(50);
-
+  
+  yoff = ystart
   for (let i = 0; i < DIM; i++) {
+    
+    xoff = xstart
     for(let j = 0; j < DIM; j++) {
       const x = i * w
       const y = j * w
+      
+      noStroke()
+      fill(noise(xoff, yoff) * 255)
       rect(x, y, w)
+
+      xoff += inc
     }
+
+    yoff += inc
   }
+
+  xstart += inc
+  ystart += inc
+  // noLoop()
 }
