58b944b931f3014f76827b052a3ca38027c7eeb2
jsc-264 <joestephen264@gmail.com>
2026-02-11 11:26:33 +0000

    adjusted grid to non circles

--- grid/sketch.js
+++ grid/sketch.js
@@ -1,9 +1,8 @@
-const DIM = 3
+const DIM = 15
 let tileSize
 
 function setup() {
   createCanvas(600, 600);
-  rectMode(CENTER)
 
   tileSize = width / DIM
 }
@@ -13,11 +12,7 @@ function draw() {
 
   for (let i = 0; i < DIM; i++) {
     for (let j = 0; j < DIM; j++) {
-      const strokeThickness = map(mouseX, 0, width, 0, tileSize * 0.5)
-      strokeWeight(strokeThickness)
-      stroke(100, 10, 250, 120)
-      fill(200, 0, 100)
-      circle(tileSize * i + tileSize/2, tileSize * j + tileSize/2, tileSize * 0.5)
+      rect(tileSize * i, tileSize * j, tileSize)
     }
   }
 }
