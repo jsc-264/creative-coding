c2d07b1ddf854a8aca3fe9af75012f97592051f6
jsc-264 <joestephen264@gmail.com>
2026-02-11 14:39:17 +0000

    defined drawSquare function

--- vera-molnar/sketch.js
+++ vera-molnar/sketch.js
@@ -1,6 +1,26 @@
 const DIM = 10
 let w
 
+function drawSquare(x, y, w) {
+  const layers = random(4, 6)
+
+  noFill()
+  rect(x, y, w)
+
+  for (let i = 0; i < layers; i++) {
+    if (random(1) < 0.7) {
+
+      const div = map(i, 0, layers, 1.2, 6)
+
+      push()
+      translate(x, y)
+      rotate(random(-20 / layers, 20 / layers))
+      rect(0, 0, w / div)
+      pop()
+    }
+  }
+}
+
 function setup() {
   createCanvas(600, 600);
   rectMode(CENTER)
