64eacdd478a11d27356a7c27de15a55bd9b4361d
jsc-264 <joestephen264@gmail.com>
2026-02-11 09:50:41 +0000

    preloaded tile images

--- truchet-tiles/sketch.js
+++ truchet-tiles/sketch.js
@@ -1,3 +1,13 @@
+let tileImages = []
+const DIM = 10
+
+function preload() {
+  tileImages = [
+    loadImage("./assets/truchet-tl-br.svg"),
+    loadImage("./assets/truchet-tr-bl.svg")
+  ]
+}
+
 function setup() {
   createCanvas(400, 400);
 }
