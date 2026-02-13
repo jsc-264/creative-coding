0e0f69a855b2dcc68797682d5df6b3d0db50ce62
jsc-264 <joestephen264@gmail.com>
2026-02-13 15:00:05 +0000

    DV: drew grid, init vars

--- experiments/data-vis/sketch.js
+++ experiments/data-vis/sketch.js
@@ -1,7 +1,22 @@
+const DIMENSION = 10
+let tileStates
+let tileSize
+
+
 function setup() {
   createCanvas(400, 400);
+  tileSize = width / DIMENSION
 }
 
 function draw() {
   background(220);
+
+  // draw grid
+  for (let i = 0; i < DIMENSION; i++) {
+    for(let j = 0; j < DIMENSION; j++) {
+      const x = i * tileSize
+      const y = j * tileSize
+      rect(x, y, tileSize)
+    }
+  }
 }
