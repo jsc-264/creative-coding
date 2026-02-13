f9fb375dace54432c2b3847d0fea53e32c7e8788
jsc-264 <joestephen264@gmail.com>
2026-02-13 15:01:43 +0000

    DV: init tile values

--- experiments/data-vis/sketch.js
+++ experiments/data-vis/sketch.js
@@ -2,10 +2,17 @@ const DIMENSION = 10
 let tileStates
 let tileSize
 
-
 function setup() {
   createCanvas(400, 400);
   tileSize = width / DIMENSION
+
+  // initialise grid
+  tileStates = new Array(DIMENSION)
+  for (let i = 0; i < DIMENSION; i++){
+    tileStates[i] = new Array(DIMENSION)
+  }
+
+  print(tileStates)
 }
 
 function draw() {
