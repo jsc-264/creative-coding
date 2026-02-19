d891fac939f2dc68df791189e878c532a4169371
jsc-264 <joestephen264@gmail.com>
2026-02-19 16:14:29 +0000

    ARW: added changing thickness prism

--- experiments/exper-1/artwork/sketch.js
+++ experiments/exper-1/artwork/sketch.js
@@ -1,7 +1,37 @@
+let offsets = {
+  triangleX: 1
+}
+
+function drawTriangle(x, y, size = 150) {
+  const points = {
+    a: createVector(0, 0),
+    b: createVector(size, 0),
+    c: createVector(size / 2, -(size * sqrt(3)) / 2),
+  }
+
+  let weight = map(noise(offsets.triangleX), 0, 1, 1, 7)
+
+  push()
+
+  translate(x - size / 2, y + size / 2)
+  noFill()
+  strokeWeight(weight)
+  stroke(255)
+  triangle(
+    points.a.x, points.a.y,
+    points.b.x, points.b.y,
+    points.c.x, points.c.y,
+  )
+  pop()
+}
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
-  background(220);
+  background(0);
+  drawTriangle(width / 2, height / 4)
+
+  offsets.triangleX += 0.01
 }
