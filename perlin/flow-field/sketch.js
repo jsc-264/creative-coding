353d0b2c55791de715865f4a99f6e0e8669a1bfb
jsc-264 <joestephen264@gmail.com>
2026-02-20 15:13:31 +0000

    created 2d Perlin noise field

--- perlin/flow-field/sketch.js
+++ perlin/flow-field/sketch.js
@@ -1,7 +1,33 @@
+const DIM = 20
+let scl
+
+let xoff, yoff
+
 function setup() {
   createCanvas(400, 400);
+  scl = width / DIM
+  noStroke()
 }
 
 function draw() {
-  background(220);
+  yoff = 0
+
+  for (let j = 0; j < DIM; j++) {
+    xoff = 1
+    for (let i = 0; i < DIM; i++) {
+      const x = i * scl
+      const y = j * scl
+
+      const shade = noise(xoff, yoff) * 255
+
+      fill(shade)
+      rect(x, y, scl)
+
+      xoff += 0.1
+    }
+
+    yoff += 0.1
+  }
+
+  noLoop()
 }
