56f0e467169a79ba198ffc7ddb2b71c63252363a
jsc-264 <joestephen264@gmail.com>
2026-02-27 21:22:14 +0000

    created body object

--- physics/sketch.js
+++ physics/sketch.js
@@ -1,7 +1,25 @@
+let b
+
+class Body {
+  constructor(x, y, w, h){
+    this.pos = createVector(x, y)
+    this.dim = {
+      width: w,
+      height: h
+    }
+  }
+
+  render() {
+    rect(this.pos.x, this.pos.y, this.dim.width, this.dim.height)
+  }
+}
+
 function setup() {
   createCanvas(400, 400);
+  b = new Body(100, 100, 50, 50)
 }
 
 function draw() {
   background(220);
+  b.render()
 }
