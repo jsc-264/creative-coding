53d1e1de32e053b4bddfff80f39dade24893c77f
jsc-264 <joestephen264@gmail.com>
2026-03-04 10:02:56 +0000

    drawn image to canvas

--- Image/sketch.js
+++ Image/sketch.js
@@ -1,7 +1,15 @@
+let img;
+
+function preload() {
+  img = loadImage("parrot.jpeg")
+}
+
 function setup() {
   createCanvas(400, 400);
 }
 
 function draw() {
   background(220);
+
+  image(img, 0, 0)
 }
