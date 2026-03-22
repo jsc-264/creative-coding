98a83a06958a9dde5d935279322a4b65f611e688
jsc-264 <joestephen264@gmail.com>
2026-03-22 14:32:04 +0000

    FFT: absrtacted wave drawing to function

--- experiments/exper-2/FFT/sketch.js
+++ experiments/exper-2/FFT/sketch.js
@@ -12,28 +12,35 @@ function setup() {
   fft = new p5.FFT(0.5, 128);
   fft.setInput(song)
   fill(0)
-  noStroke()
 }
 
 function draw() {
   background(220);
+  const spectrum = fft.analyze()
 
   if (song.isPlaying()) {
     text("Playing", 10, 20);
-
-    const spectrum = fft.analyze()
-
-    const w = width / spectrum.length
-
-    for (let i = 0; i < spectrum.length; i++) {
-      let amp = spectrum[i];
-      let x = w * i
-      let y = map(amp, 0, 256, height, 0);
-      rect(x, y, w, height-y);
-    }
-
+    wave(spectrum)
   } else {
     text("Paused", 10, 20);
+  }
+}
+
+function wave(spectrum, x, y, w, h) {
+  for (let i = spectrum.length - 1; i >= 0; i--) {
+    let amp = spectrum[i]
+    if (amp <= 2) {
+      spectrum.splice(i, 1)
+    }
+  }
+
+  const ampW = width / spectrum.length
+
+  for (let i = 0; i < spectrum.length; i++) {
+    let amp = spectrum[i];
+    let ampX = w * i
+    let ampY = map(amp, 0, 256, height, 0);
+    rect(ampX, y, ampW, height - y);
   }
 }
 
