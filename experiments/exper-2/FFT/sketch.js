c7dc5de85db2ebf220ff2b3ee6c5fbe8dff5ded6
jsc-264 <joestephen264@gmail.com>
2026-03-22 14:14:35 +0000

    FFT: displayed spectrum as wave

--- experiments/exper-2/FFT/sketch.js
+++ experiments/exper-2/FFT/sketch.js
@@ -1,6 +1,5 @@
 let song;
 let fft;
-const bins = 1024
 
 function preload() {
   song = loadSound("./song.mp3");
@@ -10,8 +9,10 @@ function setup() {
   createCanvas(400, 400);
   song.play();
 
-  fft = new p5.FFT();
+  fft = new p5.FFT(0.5, 128);
   fft.setInput(song)
+  fill(0)
+  noStroke()
 }
 
 function draw() {
@@ -21,6 +22,15 @@ function draw() {
     text("Playing", 10, 20);
 
     const spectrum = fft.analyze()
+
+    const w = width / spectrum.length
+
+    for (let i = 0; i < spectrum.length; i++) {
+      let amp = spectrum[i];
+      let x = w * i
+      let y = map(amp, 0, 256, height, 0);
+      rect(x, y, w, height-y);
+    }
 
   } else {
     text("Paused", 10, 20);
