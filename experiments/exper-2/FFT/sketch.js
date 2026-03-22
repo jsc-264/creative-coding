ff787af933812c855457ec26f16736be05d13054
jsc-264 <joestephen264@gmail.com>
2026-03-22 13:48:28 +0000

    FFT: loaded song into sketch

--- experiments/exper-2/FFT/sketch.js
+++ experiments/exper-2/FFT/sketch.js
@@ -1,7 +1,30 @@
+let song;
+
+function preload() {
+  song = loadSound("./song.mp3");
+}
+
 function setup() {
   createCanvas(400, 400);
+  song.play();
 }
 
 function draw() {
-  background(220)
+  background(220);
+
+  if (song.isPlaying()) {
+    text("Playing", 10, 20);
+  } else {
+    text("Paused", 10, 20);
+  }
+}
+
+function keyPressed() {
+  if (key == " ") {
+    if (song.isPlaying()) {
+      song.pause();
+    } else {
+      song.play();
+    }
+  }
 }
