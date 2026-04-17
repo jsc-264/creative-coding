c0d72a93aa116a11a67db588ae77f05445ecf088
jsc-264 <joestephen264@gmail.com>
2026-04-17 15:47:24 +0100

    SYS: added flock Method to bird

--- experiments/exper-3/system/sketch.js
+++ experiments/exper-3/system/sketch.js
@@ -11,17 +11,38 @@ function setup() {
 function draw() {
   background(135, 206, 235);
   flock.forEach(bird => {
+    bird.flock(flock)
+    bird.update()
     bird.render()
   })
 }
 
 class Bird {
-  constructor(x, y){
+  constructor(x, y) {
     this.pos = createVector(x, y)
+    this.vel = p5.Vector.random2D()
     this.diam = 20
   }
 
-  render(){
+  render() {
     circle(this.pos.x, this.pos.y, this.diam)
+    line(this.pos.x, this.pos.y, this.pos.x + this.vel.x * this.diam, this.pos.y + this.vel.y * this.diam)
+  }
+
+  update() {
+    this.pos.add(this.vel)
+  }
+
+  flock(flock) {
+    for (let bird of flock) {
+      if (bird == this) {
+        continue
+      }
+
+      let d = dist(bird.pos.x, bird.pos.y, this.pos.x, this.pos.y)
+      if (d < 200) {
+        line(bird.pos.x, bird.pos.y, this.pos.x, this.pos.y)
+      }
+    }
   }
 }
