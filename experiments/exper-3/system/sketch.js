9fdacb8ec489af081157eea0bc2c2df128616af6
jsc-264 <joestephen264@gmail.com>
2026-04-17 14:47:24 +0100

    SYS: created bird class

--- experiments/exper-3/system/sketch.js
+++ experiments/exper-3/system/sketch.js
@@ -5,3 +5,13 @@ function setup() {
 function draw() {
   background(135, 206, 235);
 }
+
+class Bird {
+  constructor(x, y, dir){
+    this.pos = createVector(x, y)
+    this.dir = dir
+
+    this.vel = createVector(0, 0)
+    this.acc = createVector(0, 0)
+  }
+}
