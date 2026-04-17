c9bb3146503e38781679e90511a45d4833d2366f
jsc-264 <joestephen264@gmail.com>
2026-04-17 15:41:11 +0100

    SYS: created bird class

--- experiments/exper-3/system/sketch.js
+++ experiments/exper-3/system/sketch.js
@@ -1,8 +1,27 @@
+let flockSize = 10
+let flock = []
+
 function setup() {
   createCanvas(windowWidth, windowHeight);
-
+  for (let i = 0; i < flockSize; i++) {
+    flock.push(new Bird(random(width), random(height)))
+  }
 }
 
 function draw() {
   background(135, 206, 235);
+  flock.forEach(bird => {
+    bird.render()
+  })
+}
+
+class Bird {
+  constructor(x, y){
+    this.pos = createVector(x, y)
+    this.diam = 20
+  }
+
+  render(){
+    circle(this.pos.x, this.pos.y, this.diam)
+  }
 }
