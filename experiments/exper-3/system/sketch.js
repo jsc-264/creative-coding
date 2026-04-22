c85bb4654f6460dc3a4dfc87ed16c4a48e63d376
jsc-264 <joestephen264@gmail.com>
2026-04-22 12:21:16 +0100

    SYS: drew sliders

--- experiments/exper-3/system/sketch.js
+++ experiments/exper-3/system/sketch.js
@@ -1,13 +1,35 @@
 let flockSize = 200
 let flock = []
 
+let cohesionSlider
+let alignmentSlider
+let separationSlider
+
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  createCanvas(windowWidth*0.9, windowHeight*0.9);
   angleMode(DEGREES)
 
   for (let i = 0; i < flockSize; i++) {
     flock.push(new Bird(random(width), random(height), 10))
   }
+
+  const cohesionP = createP("Cohesion")
+  cohesionP.position(0, 0)
+
+  cohesionSlider = createSlider(10, 200, 20)
+  cohesionSlider.position(0, 0)
+
+  const alignmentP = createP("Alignment")
+  alignmentP.position(200, 0)
+
+  alignmentSlider = createSlider(10, 200, 100)
+  alignmentSlider.position(200, 0)
+
+  const separationP = createP("Separation")
+  separationP.position(400, 0)
+
+  separationSlider = createSlider(10, 200, 100)
+  separationSlider.position(400, 0)
 }
 
 function draw() {
