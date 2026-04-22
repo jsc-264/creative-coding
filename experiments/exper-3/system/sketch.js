4ab75608a8fe474894188a42e1e97dd58f45ed3f
jsc-264 <joestephen264@gmail.com>
2026-04-22 10:49:55 +0100

    SYS: added align method to bird

--- experiments/exper-3/system/sketch.js
+++ experiments/exper-3/system/sketch.js
@@ -20,14 +20,14 @@ function draw() {
   })
 }
 
-function averageAngle(bird1, bird2) {
-  const thisNormVec = p5.Vector.normalize(bird1.vel)
-  const birdNormVec = p5.Vector.normalize(bird2.vel)
+// function averageAngle(bird1, bird2) {
+//   const thisNormVec = p5.Vector.normalize(bird1.vel)
+//   const birdNormVec = p5.Vector.normalize(bird2.vel)
 
-  const avgVec = thisNormVec.add(birdNormVec).normalize()
+//   const avgVec = thisNormVec.add(birdNormVec).normalize()
 
-  return avgVec
-}
+//   return avgVec
+// }
 
 class Bird {
   constructor(x, y, size) {
@@ -35,7 +35,7 @@ class Bird {
     this.vel = createVector(random(-1, 1), random(-1, 1))
     this.size = size
 
-    this.speed = random(1, 2)
+    this.speed = random(5, 10)
     this.maxSpeed = map(this.size, 5, 15, 6, 1)
 
     this.col = color(random(20, 100), random(20, 100), random(10, 15))
@@ -67,37 +67,32 @@ class Bird {
     if (this.pos.y > height + this.size) this.pos.y = -this.size
   }
 
-  flock(flock) {
-    // flocking has 3 main concepts within it
-    // 1) separation: close togther birds will steer away from each other if they are too close
-    // 2) alignment: close together birds will take on a similar direction and velocity (already implemented ish)
-    // 3) cohesion: close togther birds will attempt to travel to an average position of all of them
-
-    // max angle that two birds can be meeting at and they still change direction
-    // 180 = two birds will always turn if near each other
-    // 0 = two birds will never turn if near each other
-    const angleMax = 145
-
-    let numClose = 0
-
-    for (let bird of flock) {
+  align(flock) {
+    let avgVel = createVector(0, 0)
+    for (let bird of flock){
       if (bird == this) {
         continue
       }
 
       let d = dist(bird.pos.x, bird.pos.y, this.pos.x, this.pos.y)
       const closeEnough = d < 20
-      numClose += closeEnough ? 1 : 0
 
-
-      const angleDifference = this.vel.angleBetween(bird.vel)
-      const similarDirection = angleDifference < angleMax && angleDifference > -angleMax
-
-      if (closeEnough && similarDirection) {
-        this.vel = averageAngle(this, bird)
+      if (closeEnough) {
+        avgVel.add(p5.Vector.normalize(bird.vel))
       }
     }
 
-    this.speed = map(numClose, 0, 5, this.maxSpeed, 1)
+    this.vel = avgVel
+  }
+
+  flock(flock) {
+    // flocking has 3 main concepts within it
+    // 1) separation: close togther birds will steer away from each other if they are too close
+    // 2) alignment: close together birds will take on a similar direction and velocity (already implemented ish)
+    // 3) cohesion: close togther birds will attempt to travel to an average position of all of them
+
+    this.separate(flock)
+    this.align(flock)
+    this.cohere(flock)
   }
 }
